619bf8682dc185928c4dccf51748b51247a80a1d
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2023-08-29 14:58:35 +0530

    Update wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -13,4 +13,17 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
-application = get_wsgi_application()
+from portP import MyWSGIApp
+
+
+
+# application = get_wsgi_application()
+
+# app = application
+
+
+
+
+application = MyWSGIApp()
+application = WhiteNoise(application, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
