687dad8803dc0aeaf6e1d3b865cadf7480a3ff2e
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2023-08-29 14:19:33 +0530

    Update views.py

--- porapp/views.py
+++ porapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
+from django.utils.datastructures import MultiValueDictKeyError
 
-# Create your views here.
+def index(request):
+    if request.method == "POST" :
+        a = port()
+        a.name=request.POST['name']
+        a.mail=request.POST['mail']
+        a.subject=request.POST['subject']
+        a.mesage = request.POST['mesage']
+    
+    return render(request, 'index.html')
